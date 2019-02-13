6e5f57388ba91786d8cc221a2e4a56e272d21031
Peter Rose <pwrose@ucsd.edu>
2019-02-12 19:18:24 -0800

    updated Dockerfile

---
+++ vice/entry.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' | envsubst > $HOME/.irods/irods_environment.json
+cd $HOME
+exec jupyter lab --no-browser
