6769426ba23d9e0526130d2b79a280355d499634
Peter Rose <pwrose@ucsd.edu>
2019-06-18 19:46:11 -0700

    added view image option

--- vice/entry.sh
+++ vice/entry.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' | envsubst > $HOME/.irods/irods_environment.json
-cd $HOME
-exec jupyter lab --no-browser
+cd $HOME/vice
+mv $HOME/mmtf-genomics $HOME/vice/
+exec jupyter lab --no-browser $HOME/vice/mmtf-genomics/pipeline1/1-ReadMutations.ipynb
