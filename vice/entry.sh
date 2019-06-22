d3327dd7af9ab78ee3910de4fd328baadc06ebae
Peter Rose <pwrose@ucsd.edu>
2019-06-21 17:40:07 -0700

    lauch from default location

--- vice/entry.sh
+++ vice/entry.sh
@@ -3,4 +3,5 @@
 echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' | envsubst > $HOME/.irods/irods_environment.json
 cd $HOME/vice
 mv $HOME/mmtf-genomics $HOME/vice/
-exec jupyter lab --no-browser $HOME/vice/mmtf-genomics/pipeline1/1-ReadMutations.ipynb
+exec jupyter lab --no-browser
+# exec jupyter lab --no-browser $HOME/vice/mmtf-genomics/pipeline1/1-ReadMutations.ipynb (launches in pipeline1 directory)
